87945dbe98a67438c61d089fafe914f3a8125887
Kalina Allen <kalina.allen@gmail.com>
2020-08-26 10:27:45 -1000

    Created basic intereactive frontend for todo app, switched to two space indentation

--- app/components/App.js
+++ app/components/App.js
@@ -1,41 +1,104 @@
 import React from 'react';
 
-class Button extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {numberOfClicks: 0};
-    
-        // This binding is necessary to make `this` work in the callback
-        this.handleClick = this.handleClick.bind(this)
-    }
-
-    handleClick() {
-        this.setState((state, props) => ({
-            numberOfClicks: state.numberOfClicks + 1
-        }));
-    }
-
-    render() {
-        const numClicks = this.state.numberOfClicks
-        let buttonText;
-        if (numClicks == 0) {
-            buttonText = "Click me!";
-        } else {
-            buttonText = "This button has been clicked " + numClicks + " times"
-        }
-
-        return (
-            <button onClick = {this.handleClick}>
-                {buttonText}
-            </button>
-        );
-
-
-    }
+class ListItem extends React.Component {
+  render() {
+    return(
+      <li>
+        <input type="checkbox"></input>
+        {this.props.text}
+      </li>
+    );
+  }
 }
-  
+
+class ItemList extends React.Component {
+  render() {
+    return (
+      <ul style={{listStyle: "none"}}>
+        {this.props.items.map((item) =>
+          <ListItem text={item} />
+        )}
+      </ul>
+    );
+  }
+}
+
+class NewItemInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.props.onNewItemTextChange(event.target.value);
+  }
+
+  handleSubmit(event) {
+    this.props.onSubmit();
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input 
+          type="text"
+          placeholder="Wash the laundry..."
+          value={this.props.newItemText}
+          onChange={this.handleChange} />
+        <input type="submit" value="Submit"/>
+      </form>
+    );
+  }
+}
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {items: props.items, newItemText: ""};
+
+    this.onNewItemTextChange = this.onNewItemTextChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onNewItemTextChange(value) {
+    this.setState((state, props) => ({
+      newItemText: value
+    }));
+  }
+
+  onSubmit(value) {
+    this.setState((state) => {
+      state.items.push(state.newItemText);
+      return {
+        items: state.items,
+        newItemText: ""
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>My ToDo List</h1>
+        <>
+          <NewItemInput onNewItemTextChange={this.onNewItemTextChange}
+                        onSubmit={this.onSubmit}
+                        newItemText={this.state.newItemText} />
+          <ItemList items={this.state.items} />
+        </>
+      </div>
+    );
+  }
+}
+
 function App() {
-    return <Button />;
+  return (
+    <TodoList items={["item1", "item2"]} />
+  );
 }
+
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
 // webpack.config.js
 var HTMLWebpackPlugin = require('html-webpack-plugin')
 var HTMLWebpackPluginConfig = new HTMLWebpackPlugin ({
-    template: __dirname + '/app/index.html',
-    filename: 'index.html',
-    inject: 'body'
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
 });
 module.exports = {
-    mode: 'development',
-    entry: __dirname + '/app/index.js',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            }
-        ]
-    },
-    output: {
-      filename: 'transformed.js',
-      path: __dirname + '/build'
-    },
-    plugins: [HTMLWebpackPluginConfig]
-  };
+  mode: 'development',
+  entry: __dirname + '/app/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  output: {
+    filename: 'transformed.js',
+    path: __dirname + '/build'
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
